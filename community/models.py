aac6d794e1b46c47a7e98b8b65b5564ae171d9a8
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-07-04 23:07:26 +0300

    creating community model

--- community/models.py
+++ community/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Community(models.Model):
+    image = CloudinaryField('image', default='image')
+    description = models.TextField(blank=True, max_length=300)
+    category = models.CharField(max_length=100,default='category')
+
+
+
--- mzaziproject/settings.py
+++ mzaziproject/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import cloudinary
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,3 +130,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+cloudinary.config(
+  cloud_name = "oyesa",
+  api_key = "749352579693875",
+  api_secret = "W6qFNFY_0mRnS6YbzrzWwegcfCY",
+)
