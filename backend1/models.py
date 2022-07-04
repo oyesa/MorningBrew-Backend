9f5f251eb6063a1f9f5c873152037db131d0b28f
rachel <rachelkemuma99@gmail.com>
2022-07-05 00:01:41 +0300

    create model comment and post

--- backend1/models.py
+++ backend1/models.py
@@ -1,3 +1,4 @@
+from xml.etree.ElementTree import Comment
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
@@ -14,3 +15,23 @@ class Service(models.Model):
 
     def __str__(self):
         return self.title
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,blank=True,null=True)
+    title = models.CharField(max_length=200,blank=True)
+    description = models.TextField()
+    post_date = models.DateField(auto_now_add=True)
+    image = CloudinaryField('image', default='image')
+    # comment = models.ForeignKey(Comment,on_delete=models.CASCADE, related_name='comment',null=True)
+
+    def save_post(self):
+        self.save()
+
+    def __str__(self):
+        return self.title
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,blank=True,null=True)
+    post = models.ForeignKey(Post, related_name = "comment", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.comment} Post'
--- backend1/serializer.py
+++ backend1/serializer.py
@@ -1,9 +1,22 @@
 from dataclasses import fields
 from statistics import mode
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import *
+from django.contrib.auth.models import User
 
-class ServiceSerializer(ModelSerializer):
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = [
+#             'username', 
+#             'first_name', 
+#             'last_name', 
+#             'id',
+#         ]
+
+class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Service
-        fields = '__all__'
+        # fields = '__all__'
+        fields = ['title','image','user','description']
