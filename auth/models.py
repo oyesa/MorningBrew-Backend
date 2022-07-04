4e8df6dcd321d2fed5969ddca291acdf3f117083
@cheeks cloud <agneskaswii700@gmail.com>
2022-07-04 16:05:11 +0300

    cteating model for user registration token

--- auth/models.py
+++ auth/models.py
@@ -32,3 +32,41 @@ class UserManager(BaseUserManager):
       user.save()
 
       return user
+ 
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(db_index=True, max_length=255, unique=True)
+    email = models.EmailField(db_index=True, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    first_name = models.CharField(('first name'), max_length=30, blank=True)
+  
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    #since we do not store their real name we return their username
+    def get_full_name(self):
+        return self.username
+
+    #this allows us to return the jwt token by calling user.token method
+    def token(self):
+        return self.generate_jwt_token()
+
+
+      #generate token using user emailand username .token is generated during signup  
+    def generate_jwt_token(self):
+        user_details = {'email':self.email,'username':self.username}
+        token = jwt.encode(
+            {
+                'user_data':user_details,
+                'exp':datetime.now() + timedelta(hours=24)
+            },settings.SECRET_KEY,algorithm ='HS256'
+        )
+        
+        return token
