07b03edd5732c7d804feb412079e5b5bfcf3189b
rachel <rachelkemuma99@gmail.com>
2022-07-04 23:12:40 +0300

    create model service and migrate

--- backend1/models.py
+++ backend1/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Service(models.Model):
+    title = models.CharField(max_length=150)
+    image = CloudinaryField('image',null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,blank=True,null=True)
+    description = models.TextField()
+
+    def save_service(self):
+        self.save
+
+    def __str__(self):
+        return self.title
