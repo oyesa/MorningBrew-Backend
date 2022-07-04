35428606f2408502af0076e5b55e6b7793396e9e
@cheeks cloud <agneskaswii700@gmail.com>
2022-07-04 16:04:08 +0300

    cteating model for user

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,34 @@
+import jwt
+from datetime import datetime, timedelta
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, first_name, password=None):
+        if username is None:
+            raise TypeError('Users must have a username.')
+
+        if email is None:
+            raise TypeError('Users must have an email address.')
+
+        User = self.model(username=username, email=self.normalize_email(email), first_name=first_name)
+        User.set_password(password)
+        User.save()
+
+        return User
+
+    def create_superuser(self, username, email, password):
+      if password is None:
+          raise TypeError('Superusers must have a password.')
+
+      user = self.create_user(username, email, password)
+      user.is_superuser = True
+      user.is_staff = True
+      user.is_verified=True
+      user.save()
+
+      return user
--- mzaziproject/settings.py
+++ mzaziproject/settings.py
@@ -30,6 +30,9 @@ INSTALLED_APPS = [
     'rest_framework',
     'cloudinary',
     'auth',
+    'cloudinary',
+    'rest_framework.authentication',
+
 ]
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 CLOUDINARY_STORAGE = {
@@ -124,6 +127,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+   'DEFAULT_AUTHENTICATION_CLASSES': (
+       'rest_framework.authentication.TokenAuthentication',
+       'rest_framework.authentication.SessionAuthentication',
+   ),
+   'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAdminUser',
+   ),
 }
