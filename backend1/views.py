8900613f3be28e8ad7fcf0d5daf1725476f00e06
rachel <rachelkemuma99@gmail.com>
2022-07-04 23:18:41 +0300

    create viewset for the service

--- backend1/views.py
+++ backend1/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import *
+from .serializer import *
 
 # Create your views here.
+class ServiceViewSet(ModelViewSet):
+    serializer_class=ServiceSerializer
+    queryset=Service.objects.all()
