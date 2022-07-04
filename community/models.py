c6ad7c8250c13c3f0df39b72890b3cec3d33ae17
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-07-05 01:44:16 +0300

    creating serializers for the community model

--- community/models.py
+++ community/models.py
@@ -2,10 +2,21 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+
 class Community(models.Model):
+    CATEGORY =(
+        ('share your experience','share your experience'),
+        ('Join a mzazi space event','Join a mzazi space event'),
+        ('Support and Services','Support and Services'),
+        ('Single parent Groups','Single parent Groups'),
+        
+    )
+    
     image = CloudinaryField('image', default='image')
     description = models.TextField(blank=True, max_length=300)
-    category = models.CharField(max_length=100,default='category')
+    category = models.CharField(max_length=50, null=True, choices=CATEGORY)
+
 
 
 
--- community/urls.py
+++ community/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
+from django.urls import path,include
+from rest_framework import routers
+from django.conf import settings
+from .views import *
+from . import views
+
+
+router = routers.DefaultRouter()
+router.register('community', views.CommunityViewSet)
 
 urlpatterns = [
-
+    path('api/',include(router.urls)),
+    path('api/community/<int:pk>/',views.showcommunity, name='community')
 
 ]
